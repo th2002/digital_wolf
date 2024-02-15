dc53da30aa233b193acd12fa168db2dd64e5b064
Đinh Lê Trung Hiếu <dinhletrunghieu0207@gmail.com>
2024-02-16 00:49:28 +0700

    style: change theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
         <div className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
           <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
             Your marketplace for high-quality{" "}
-            <span className="text-blue-600">digital assets</span>.
+            <span className="text-primary">digital assets</span>.
           </h1>
           <p className="mt-6 text-lg max-w-prose text-muted-foreground">
             Welcome to DigitalWolf. Every assets on our platform are is verified
